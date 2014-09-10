32f3fd66df0d74594947b187835dbc40df73e81b
Pedro Beja <althaser@gmail.com>
2014-09-10 23:38:18 +0100

    add new screenshot

---
+++ menu.php
@@ -0,0 +1,61 @@
+<?php
+  session_start();
+  if(!isset($_SESSION['id'])) { header("Location: index.php"); exit (0); }
+?>
+
+<div class="container">
+  <div class="content">
+    <ul class="ca-menu">
+      <li><a href="#">
+            <span class="ca-icon">X</span>
+            <div class="ca-content">
+              <h2 class="ca-main">Drinks</h2>
+              <h3 class="ca-sub">bebidas</h3>
+            </div>
+          </a>
+      </li>
+      <li><a href="#">
+            <span class="ca-icon">H</span>
+            <div class="ca-content">
+              <h2 class="ca-main">Starters</h2>
+              <h3 class="ca-sub">entradas</h3>
+            </div>
+          </a>
+      </li>
+      <li><a href="#">
+            <span class="ca-icon">N</span>
+            <div class="ca-content">
+              <h2 class="ca-main">Pizzas</h2>
+              <h3 class="ca-sub">pizzas</h3>
+            </div>
+          </a>
+      </li>
+      <li><a href="#">
+            <span class="ca-icon">K</span>
+            <div class="ca-content">
+              <h2 class="ca-main">Pasta</h2>
+              <h3 class="ca-sub">massas</h3>
+            </div>
+          </a>
+      </li>
+      <li><a href="#">
+            <span class="ca-icon">O</span>
+            <div class="ca-content">
+              <h2 class="ca-main">Salads</h2>
+              <h3 class="ca-sub">saladas</h3>
+            </div>
+          </a>
+      </li>
+      <li><a href="#">
+            <span class="ca-icon">R</span>
+            <div class="ca-content">
+              <h2 class="ca-main">Disserts</h2>
+              <h3 class="ca-sub">sobremesas</h3>
+            </div>
+          </a>
+      </li>
+    </ul>
+  </div>
+</div>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+
